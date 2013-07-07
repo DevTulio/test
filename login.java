90f4c0e59d32b13b4f24ba5d7261473dfc564e8e
DevTulio <mikeavalardo1@hotmail.com>
2013-07-07 12:17:55 +0800

    commit

---
+++ login.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+
+class login
+
+{	public static void main(String[] a) throws IOException
+	{
+		BufferedReader inp=new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("Enter PAssword");
+
+		String an;
+
+		an=inp.readLine();
+
+	}
+	
+
+}
